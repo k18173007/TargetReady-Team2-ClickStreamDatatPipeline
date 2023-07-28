c0dbe74171ed41fd4c1a2c6c6a9efd50d60c6a51
k18173007 <67572885+k18173007@users.noreply.github.com>
2023-07-28 23:08:40 +0530

    Starting Commit from Krishna 28/07/23

---
+++ src/test/scala/com/target_ready/data/pipeline/consumer/Consumer.scala
@@ -0,0 +1,150 @@
+package com.target_ready.data.pipeline.consumer
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.Trigger._
+object Consumer {
+  def main(args: Array[String]) {
+
+   /** Creating Spark Session */
+
+    val spark = SparkSession.builder()
+      .appName("TargetReady-Team2-ClickStreamDatatPipeline")
+      .master("local[*]")
+      .getOrCreate()
+
+
+    import spark.implicits._
+
+    /** Subscribing to the topic and reading data from stream */
+
+    val df = spark                                                                   /** Spark Session */
+      .readStream                                                                    /** DataStream Reader*/
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "clickStream")
+      .option("startingOffsets", "earliest")
+      .option("failOnDataLoss","false")
+      .load()                                                                        /** Converting StreamData into DataFrame */
+
+
+      /** Casting Dataframe into Dataset [(String, String)] format */
+
+      df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
+
+
+      /** Saving the Streamed Data */
+
+      df.writeStream                                                                  /** DataStream Writer */
+      .outputMode("append")
+      .format("orc")
+      .option("path", "data/output")                                                  /** Directory path for saving the data */
+      .option("checkpointLocation", "FQDN")
+      .trigger(ProcessingTime("30 seconds"))                                  /** Refreshing the Consumer in fixed time Interval */
+      .start()
+      .awaitTermination()                                                             /** Stopping the Termination */
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.functions.{col, from_json}
+//import org.apache.spark.sql.types.{IntegerType, StructField, StringType, StructType}
+//
+//object Consumer {
+//  def main(args:Array[String]): Unit= {
+//    val spark: SparkSession = SparkSession.builder()
+//      .master("local[3]")
+//      .appName("Spark1")
+//      .getOrCreate()
+//
+//    spark.sparkContext.setLogLevel("ERROR")
+//
+//    val df = spark.readStream
+//      .format("kafka")
+//      .option("kafka.bootstrap.servers", "192.168.1.100:9092")
+//      .option("subscribe", "test1")
+//      .option("startingOffsets", "earliest") // From starting
+//      .load()
+//
+////      .awaitTermination()
+//
+////    df.printSchema()
+//    df.show()
+//
+////df.awaitTermination()
+//    //df.show(false)
+//    //org.apache.spark.sql.AnalysisException: Queries with streaming sources must be executed with writeStream.start();;
+//
+////    val schema = StructType(
+////      List(
+////        StructField("item_id", StringType, true),
+////        StructField("item_price", StringType, true),
+////        StructField("product_type", StringType, true),
+////        StructField("department_name", StringType, true)
+////      )
+////    )
+////
+////
+////
+////    val person = df.selectExpr("CAST(value AS STRING)")
+////      .select(from_json(col("value"), schema).as("data"))
+////      .select("data.*")
+////
+////    /**
+////     *uncomment below code if you want to write it to console for testing.
+////     */
+////    //    val query = person.writeStream
+////    //      .format("console")
+////    //      .outputMode("append")
+////    //      .start()
+////    //      .awaitTermination()
+////
+////    /**
+////     *uncomment below code if you want to write it to kafka topic.
+////     */
+////    df.writeStream
+////      .format("kafka")
+////      .outputMode("append")
+////      .option("kafka.bootstrap.servers", "192.168.1.100:9092")
+////      .option("topic", "test1")
+////      .option("checkpointLocation","FQDN")
+////      .start()
+////      .awaitTermination()
+////    df.show()
+//  }
+//}
+//
